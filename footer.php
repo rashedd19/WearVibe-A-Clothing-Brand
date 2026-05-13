01643dd18c06bd0fa8b80c8d5b1b6e5a928e7103
Rashedul Islam <rashedislam.e360@gmail.com>
2026-05-14 02:13:06 +0600

    Rashedul-232400046

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer style="padding: 40px; text-align: center; background: #f9f9f9; border-top: 1px solid #eee; margin-top: 50px;">
+        <p>&copy; 2026 WearVibe. All Rights Reserved.</p>
+    </footer>
+
+</body>
+</html>
